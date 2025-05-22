c980c77e1d1ff803fabf41b2e8a4c490e3325431
Valher7 <Valentin.herman.pro@gmail.com>
2025-05-22 12:23:21 +0200

    Main entry programm class

--- src/main/java/dev/val/Main.java
+++ src/main/java/dev/val/Main.java
@@ -1,9 +1,25 @@
 package dev.val;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
+
+        List<CsvRead.CsvRow> records = CsvRead.readFile("src\\main\\Ressources\\covid_and_trade.csv");
+
+        Commands command = new Commands(records);
+        CommandsHandler processor = new CommandsHandler(command);
+
+        System.out.println("Enter help to enter the programm!");
+
+        while(true){
+            System.out.print("> ");
+            String input = scanner.nextLine().toUpperCase();
+            if(input.equals("QUIT"))break;
+            processor.execute(scanner, input);
+        }
         scanner.close();
     }
 }
